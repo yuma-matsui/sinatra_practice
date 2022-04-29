aa30a3f2e7b549915455cab529f50f3eab218492
yuma-matsui <oshino.iiyo@gmail.com>
2022-04-29 11:41:02 +0900

    新規作成、編集、削除機能の実装

---
+++ helpers/params_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+helpers do
+  def params_blank?(params)
+    params[:title].empty? || params[:content].empty?
+  end
+
+  # 投稿内容から改行を取り除く
+  def formated_params(params)
+    if params.each_value { |val| val.include?("\n") }
+      params.each do |key, val|
+        params[key] = val.chars.reject { |chr| (chr == "\r") || (chr == "\n") }.join
+      end
+    end
+    params
+  end
+
+  def remove_unnecessary_entries(params)
+    params.select { |key| (key == 'title') || (key == 'content') }
+  end
+end
