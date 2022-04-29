bee61604ac3631733ee02652ff485da311dcda79
yuma-matsui <oshino.iiyo@gmail.com>
2022-04-29 18:45:01 +0900

    XSS対応 htmlのエスケープメソッド実装

--- helpers/params_helper.rb
+++ helpers/params_helper.rb
@@ -5,17 +5,20 @@ helpers do
     params[:title].empty? || params[:content].empty?
   end
 
-  # 投稿内容から改行を取り除く
   def formated_params(params)
     if params.each_value { |val| val.include?("\n") }
       params.each do |key, val|
         params[key] = val.chars.reject { |chr| (chr == "\r") || (chr == "\n") }.join
       end
     end
-    params
+    escape_html(params)
   end
 
   def remove_unnecessary_entries(params)
     params.select { |key| (key == 'title') || (key == 'content') }
+  end
+
+  def escape_html(params)
+    params.each { |key, val| params[key] = Rack::Utils.escape_html(val) }
   end
 end
