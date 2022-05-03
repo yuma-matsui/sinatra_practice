614196767379310ccd334bd5f8ad9d93e018c6e5
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-03 21:01:44 +0900

    SQLクエリのSanitizing処理追加

--- helpers/crud_helper.rb
+++ helpers/crud_helper.rb
@@ -2,15 +2,24 @@
 
 helpers do
   def create_memo(memo)
-    @mymemo_db.exec("INSERT INTO memo (title, content) VALUES ('#{escape(memo['title'])}', '#{escape(memo['content'])}');")
+    memo = {
+      title: escape(memo['title']),
+      content: escape(memo['content'])
+    }
+    @mymemo_db.exec('INSERT INTO memo (title, content) VALUES (?, ?);', memo.values)
   end
 
   def edit_memo(edited_memo)
-    @mymemo_db.exec("UPDATE memo SET title = '#{edited_memo['title']}', content = '#{escape(edited_memo['content'])}' WHERE id = '#{escape(@memo['id'])}'")
+    memo = {
+      title: edited_memo['title'],
+      content: edited_memo['content'],
+      id: @memo['id']
+    }
+    @mymemo_db.exec_params('UPDATE memo SET title = $1, content = $2 WHERE id = $3;', memo.values)
   end
 
   def delete_memo
-    @mymemo_db.exec("DELETE FROM memo WHERE id = '#{escape(@memo['id'])}'")
+    @mymemo_db.exec('DELETE FROM memo WHERE id = $1;', [escape(@memo['id'])])
   end
 
   def escape(string)
