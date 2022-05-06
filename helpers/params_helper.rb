6745b5fddb7b3aa8c115e4593cf3e916caca29a6
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-07 08:41:56 +0900

    不要コメントの削除#消し忘れ

--- helpers/params_helper.rb
+++ helpers/params_helper.rb
@@ -4,8 +4,4 @@ helpers do
   def params_blank?(params)
     params['title'].empty? || params['content'].empty?
   end
-
-  # def remove_unnecessary_entries(params)
-  #   params.select { |key| (key == 'title') || (key == 'content') }
-  # end
 end
